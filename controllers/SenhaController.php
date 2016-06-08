31778dd42a79e1193e1afeda4189e3fe1654d2b2
SlowHusky <slowhusky@gmail.com>
2016-06-08 18:59:25 -0300

    Correção da view e criação de novas ações.

--- controllers/SenhaController.php
+++ controllers/SenhaController.php
@@ -3,12 +3,33 @@
 class Porteiro_SenhaController extends Zend_Controller_Action
 {
 	
-	public function	init(){}
+//	public function	init(){}
 
 	public function indexAction()
 	{
 		echo "<p>Teste</p>";
 	}
+
+
+
+	public function cadastrarAction(){
+		$this->view->text = "OI!";
+	}
+
+	public function removerAction(){
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 	
 
 }
