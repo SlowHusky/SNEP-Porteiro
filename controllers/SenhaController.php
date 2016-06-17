065a1d773bbe8782bef765462e46e8a9330108d5
SlowHusky <slowhusky@gmail.com>
2016-06-17 12:12:05 -0300

    tentativa de post

--- controllers/SenhaController.php
+++ controllers/SenhaController.php
@@ -17,7 +17,6 @@ class Porteiro_SenhaController extends Zend_Controller_Action
 
 	public function cadastrarAction(){
 		$this->view->breadcrumb = $this->view->translate("Porteiro >> Cadastrar nova senha");
-		$this->view->text = "OI!";
 		
 		// Parse do arquivo de formulário.
 	        $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
@@ -25,41 +24,29 @@ class Porteiro_SenhaController extends Zend_Controller_Action
 
 		// Define o título da página, é mostrado automaticamente na view.
 	        $this->view->breadcrumb = $this->view->translate("Porteiro » Cadastro");
+
 	        // Cria objeto Snep_Form
-	        $form = new Snep_Form( $xml);
+	        $form = new Zend_Form($xml);
+		$form->setMethod('post');
+		$form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
 	
-	        // Verifica se existe dados sendo enviados via $_POST
-        	// Se for verdadeiro, é porque o formulário foi submetido.
-        	if ($this->_request->getPost()) {
+		$request = $this->getRequest();
 
-			$form_isValid = true;
-			$isValid = $form->isValid($_POST);
-			$password = $form->getValue('senha');
-			$password2 = $form->getValue('senha2');
-/*
-			if ($password <> $password2){
-				//$this->view->error_message = $this->view->translate('Senhas não conferem');
-            			echo '<script language="javascript">';
-           			echo 'alert("Senhas não conferem")';
-            			echo '</script>';
-				//$this->_redirect($this->getRequest()->getControllerName());
-				$r = new Zend_Controller_Action_Helper_Redirector;
-				$r->gotoUrl('/some/url')->redirectAndExit();
-				$form_isValid = false;
-				$isValid = false;				
-			}
-*/
-			if ($form_isValid == true){
-            			echo '<script language="javascript">';
-           			echo 'alert("Senhas não conferem")';
-            			echo '</script>';                 		
-				$this->view->message = $this->view->translate('Enviado com sucesso!');
-	
-			//	$this->_redirect($this->getRequest()->getControllerName());
-			}
-		}
-
-             	$this->view->form = $form;
+    		
+        	if (getInstance()->getRequest()->isPost()) {
+        		$this->view->form = $form;
+        		$values = $form->getValues();
+			$senha1 = $form['senha'];
+			$senha2 = $form['senhac'];
+			if ($senha1 === $senha2) {
+				$this->_redirect($this->getRequest()->getControllerName() . "/confirmado");
+			}   
+          	} 
+ 
+		else {
+           		echo 'Invalid Form'; 
+      		}
+				
 	}
 }
 
