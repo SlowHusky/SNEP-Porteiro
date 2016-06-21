bd369332b9a305f6a328f4a0eb9f44938de62a89
SlowHusky <slowhusky@gmail.com>
2016-06-21 18:12:58 -0300

    Criação das tabelas senha e porteiro no sql; pagina de erro funcionando.

--- controllers/SenhaController.php
+++ controllers/SenhaController.php
@@ -5,15 +5,11 @@ class Porteiro_SenhaController extends Zend_Controller_Action
 	
 	public function	init(){}
 
-	public function indexAction()
-	{
+	public function indexAction(){
 	        // Define a baseUrl para a rotina, utilizado nos links para demais rotinas na view
 		$this->view->url = $this->getRequest()->getModuleName() .'/'. $this->getRequest()->getControllerName();
-
 		$this->view->breadcrumb = $this->view->translate("Porteiro >> Inicio");
 	}
-	
-
 
 	public function cadastrarAction(){
 		$this->view->breadcrumb = $this->view->translate("Porteiro >> Cadastrar nova senha");
@@ -40,23 +36,52 @@ class Porteiro_SenhaController extends Zend_Controller_Action
             		// Caso tudo seja válido chama a classe (Model) para inserir o dado.
             		if( $isValid ) {
            
-				$senha1 = $_POST['senha'];
-				$senha2 = $_POST['senhac'];
-                
+				$senha1 =(int) $_POST['senha'];
+				$senha2 = (int) $_POST['senhac'];
+                		$usuario = (string) $_POST['nome'];
+				$grupo = (string) $_POST['group'];
+				
                		 	// Após remover ou nao dados redireciona para método index
                		 	// Se as senhas forem iguais, ele vai redirecionar ao index
-				if ($senha1 == $senha2){
-					$this->_redirect($this->getRequest()->getControllerName() . "/confirmado");
+				if (($senha1 == $senha2) && ($senha1 > 3 && $senha1 < 21) && ($senha2 > 3 && $senha2 < 21))  {
+
+					if ((strlen($usuario) < 100 && strlen($usuario) > 0) && (strlen($grupo) < 100 && strlen($grupo) > 0)){
+
+						$this->_redirect($this->getRequest()->getControllerName() . "/index");
+					/*
+
+					$db = Zend_Registry::get('db');
+					$select
+
+
+
+
+
+
+
+
+					*/
+					}
+					 else {
+                                		$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+		                        }
 				}
+				else {
+                                	$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+                       		}
+
            	 	}
 			else {
-				$this->_redirect($this->getRequest()->getControllerName() . "/errorcadastrar");
+				$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
 			}
         	}
 		
 
         	// Envia form para a view
         	$this->view->form = $form;
+	}
+	
+	public function errorcadastrarAction(){
 	}
 }
 
---
+++ models/DbTable/Senhas.php
@@ -0,0 +1,7 @@
+<?php
+
+class Application_Model_DbTable_Senhas extends Zend_Db_Table_Abstract{
+	
+	protected $name = 'senhas';
+
+}
