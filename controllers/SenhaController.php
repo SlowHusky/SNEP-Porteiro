f2e1130de1ef4aef6914bbe2081a9c92c9a5285e
SlowHusky <slowhusky@gmail.com>
2016-06-22 17:03:21 -0300

    Implementação inicial do edit da senha.

--- controllers/SenhaController.php
+++ controllers/SenhaController.php
@@ -16,10 +16,7 @@ class Porteiro_SenhaController extends Zend_Controller_Action
 		
 		// Parse do arquivo de formulário.
 	        $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
-                                               "/modules/porteiro/forms/cadastro.xml" );
-
-		// Define o título da página, é mostrado automaticamente na view.
-	        $this->view->breadcrumb = $this->view->translate("Porteiro » Cadastro");
+                                               "/modules/porteiro/forms/cadastro.xml");
 
 	        // Cria objeto Snep_Form
 	        $form = new Snep_Form($xml);
@@ -38,22 +35,18 @@ class Porteiro_SenhaController extends Zend_Controller_Action
            
 				$senha1 =(int) $_POST['senha'];
 				$senha2 = (int) $_POST['senhac'];
-                		$usuario = (string) $_POST['nome'];
-				$grupo = (string) $_POST['group'];
+                		$usuario = $_POST['nome'];
+				$grupo = $_POST['group'];
 				
                		 	// Após remover ou nao dados redireciona para método index
                		 	// Se as senhas forem iguais, ele vai redirecionar ao index
-				if (($senha1 == $senha2) && ($senha1 > 3 && $senha1 < 21) && ($senha2 > 3 && $senha2 < 21))  {
+				if ($senha1 === $senha2)  {
 
-					if ((strlen($usuario) < 100 && strlen($usuario) > 0) && (strlen($grupo) < 100 && strlen($grupo) > 0)){
-
-						Senha_Manager::add($_POST);
-						$this->_redirect($this->getRequest()->getControllerName() . "/index");
-					}
-					 else {
-                                		$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
-		                        }
+					Senha_Manager::add($_POST);
+					$this->_redirect($this->getRequest()->getModuleName() . "/index");
 				}
+
+
 				else {
                                 	$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
                        		}
@@ -71,6 +64,56 @@ class Porteiro_SenhaController extends Zend_Controller_Action
 	
 	public function errorcadastrarAction(){
 	}
+
+	public function editarAction(){
+		$this->view->breadcrumb = $this->view->translate("Porteiro >> Editar senha");		
+
+                // Parse do arquivo de formulário.
+                $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
+                                               "/modules/porteiro/forms/editar.xml" );
+                // Cria objeto Snep_Form
+                $form = new Snep_Form($xml);
+                $form->setMethod('post');
+                $form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
+
+                // Verifica se existe dados sendo enviados via $_POST
+                // Se for verdadeiro, é porqyue o formulário foi submetido.
+                if ($this->_request->getPost()) {
+
+                        // Chama método isValid() é confronta os dados submetidos pelo formulário.
+                        $isValid = $form->isValid($_POST);
+
+                        // Caso tudo seja válido chama a classe (Model) para inserir o dado.
+                        if( $isValid ) {
+
+                                $senha1 =(int) $_POST['senha'];
+                                $senha2 = (int) $_POST['senhac'];
+                                $usuario = $_POST['nome'];
+                                $grupo = $_POST['group'];
+
+                                // Após remover ou nao dados redireciona para método index
+                                // Se as senhas forem iguais, ele vai redirecionar ao index
+                                if ($senha1 === $senha2)  {
+
+                                        Senha_Manager::edit($_POST);
+                                        $this->_redirect($this->getRequest()->getModuleName() . "/index");
+                                }
+
+
+                                else {
+                                        $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+                                }
+
+                        }
+                        else {
+                                $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+                        }
+                }
+
+                // Envia form para a view
+                $this->view->form = $form;
+        }
+
 }
 
 ?>
--- lib/Senha/Manager.php
+++ lib/Senha/Manager.php
@@ -22,6 +22,25 @@ class Senha_Manager {
 	}
 
 
+        public static function edit($data){
+
+                $db = Zend_Registry::get('db');
+                $calendario =  date("Y-m-d H:i");
+                $insert_data = array("senha" => $data['senha'], "usuario" => $data['nome'], "grupo" => $data['group'], "cadastro" => $calendario, "atualizado" => $cal$
+
+                $db->beginTransaction();
+
+                try{
+                        $db->insert('senha', $insert_data);
+                        $db->commit();
+
+                }catch(Exception $e){
+
+                $db->rollback();
+
+                }
+        }
+
 
 
 
