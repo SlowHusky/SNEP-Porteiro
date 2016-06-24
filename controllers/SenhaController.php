137a1b15baf6adccfa8a2bc67e623a3b134c2b44
SlowHusky <slowhusky@gmail.com>
2016-06-24 11:52:01 -0300

    Arquivos corrigidos. Ainda sem remover e editar no Manager.

--- controllers/SenhaController.php
+++ controllers/SenhaController.php
@@ -1,168 +1,127 @@
 <?php
-
 class Porteiro_SenhaController extends Zend_Controller_Action
 {
-
-        public function init(){}
-
-        public function indexAction(){
-                // Define a baseUrl para a rotina, utilizado nos links para demais rotinas na view
-                $this->view->url = $this->getRequest()->getModuleName() .'/'. $this->getRequest()->getControllerName();
-                $this->view->breadcrumb = $this->view->translate("Porteiro >> Inicio");
-        }
-
-        public function cadastrarAction(){
-                $this->view->breadcrumb = $this->view->translate("Porteiro >> Cadastrar nova senha");
-
-                // Parse do arquivo de formulário
-                $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
+	
+	public function	init(){}
+	public function indexAction(){
+	        // Define a baseUrl para a rotina, utilizado nos links para demais rotinas na view
+		$this->view->url = $this->getRequest()->getModuleName() .'/'. $this->getRequest()->getControllerName();
+		$this->view->breadcrumb = $this->view->translate("Porteiro >> Inicio");
+	}
+	public function cadastrarAction(){
+		$this->view->breadcrumb = $this->view->translate("Porteiro >> Cadastrar nova senha");
+		
+		// Parse do arquivo de formulário
+        	$xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
                                               "/modules/porteiro/forms/cadastro.xml" );
-
+	        // Cria objeto Snep_Form
+	        $form = new Snep_Form($xml);
+		$form->setMethod('post');
+		$form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
+	
+        	// Verifica se existe dados sendo enviados via $_POST
+        	// Se for verdadeiro, é porqyue o formulário foi submetido.
+        	if ($this->_request->getPost()) {
+            		// Chama método isValid() é confronta os dados submetidos pelo formulário.
+         	   	$isValid = $form->isValid($_POST);
+            		// Caso tudo seja válido chama a classe (Model) para inserir o dado.
+            		if( $isValid ) {
+           
+				$senha1 =(int) $_POST['senha'];
+				$senha2 = (int) $_POST['senhac'];
+                		$usuario = $_POST['nome'];
+				$grupo = $_POST['group'];
+				
+               		 	// Após remover ou nao dados redireciona para método index
+               		 	// Se as senhas forem iguais, ele vai redirecionar ao index
+				if ($senha1 === $senha2)  {
+					Senha_Manager::add($_POST);
+					$this->_redirect($this->getRequest()->getModuleName() . "/senha/index");
+				}
+				else {
+                                	$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+                       		}
+           	 	}
+			else {
+				$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+			}
+        	}
+		
+        	// Envia form para a view
+        	$this->view->form = $form;
+	}
+	
+	public function errorcadastrarAction(){
+	}
+	public function editarAction(){
+		$this->view->breadcrumb = $this->view->translate("Porteiro >> Editar senha");
+		
+		// Parse do arquivo de formulário.
+	        $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
+                                               "/modules/porteiro/forms/editar.xml");
+	        // Cria objeto Snep_Form
+	        $form = new Snep_Form($xml);
+		$form->setMethod('post');
+		$form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
+	
+        	// Verifica se existe dados sendo enviados via $_POST
+        	// Se for verdadeiro, é porqyue o formulário foi submetido.
+        	if ($this->_request->getPost()) {
+            		// Chama método isValid() é confronta os dados submetidos pelo formulário.
+         	   	$isValid = $form->isValid($_POST);
+            		// Caso tudo seja válido chama a classe (Model) para inserir o dado.
+            		if( $isValid ) {
+           
+				$senha1 =(int) $_POST['senha'];
+				$senha2 = (int) $_POST['senhac'];
+			
+               		 	// Após remover ou nao dados redireciona para método index
+               		 	// Se as senhas forem iguais, ele vai redirecionar ao index
+				if ($senha1 === $senha2)  {
+					Senha_Manager::edit($_POST);
+					$this->_redirect($this->getRequest()->getModuleName() . "/senha/index");
+				}
+				else {
+                                	$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+                       		}
+           	 	}
+			else {
+				$this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
+			}
+        	}
+		
+        	// Envia form para a view
+        	$this->view->form = $form;
+	}
+	public function removerAction(){
+		$this->view->breadcrumb = $this->view->translate("Porteiro >> Editar senha");		
+                // Parse do arquivo de formulário.
+                $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
+                                               "/modules/porteiro/forms/remover.xml" );
                 // Cria objeto Snep_Form
                 $form = new Snep_Form($xml);
                 $form->setMethod('post');
                 $form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
-
                 // Verifica se existe dados sendo enviados via $_POST
                 // Se for verdadeiro, é porqyue o formulário foi submetido.
-                if ($this->_request->getPost()) {
-
+                
+		if ($this->_request->getPost()) {
                         // Chama método isValid() é confronta os dados submetidos pelo formulário.
                         $isValid = $form->isValid($_POST);
-
                         // Caso tudo seja válido chama a classe (Model) para inserir o dado.
                         if( $isValid ) {
-
-                                $senha1 =(int) $_POST['senha'];
-                                $senha2 = (int) $_POST['senhac'];
-                                $usuario = $_POST['nome'];
-                                $grupo = $_POST['group'];
-
-                                // Após remover ou nao dados redireciona para método index
-                                // Se as senhas forem iguais, ele vai redirecionar ao index
-                                if ($senha1 === $senha2)  {
-
-                                        Senha_Manager::add($_POST);
+                                $usuario = (string) $_POST['nome'];
+                                if (strlen($usuario) > 0){
+                                        Senha_Manager::remove($_POST);
                                         $this->_redirect($this->getRequest()->getModuleName() . "/senha/index");
-                                }
-
-
-                                else {
-                                        $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
-                                }
-
+				}
                         }
                         else {
                                 $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
                         }
                 }
-
-
                 // Envia form para a view
                 $this->view->form = $form;
         }
-
-        public function errorcadastrarAction(){
-        }
-
-        public function indexAction(){
-                // Define a baseUrl para a rotina, utilizado nos links para demais rotinas na view
-                $this->view->url = $this->getRequest()->getModuleName() .'/'. $this->getRequest()->getControllerName();
-                $this->view->breadcrumb = $this->view->translate("Porteiro >> Inicio");
-        }
-
-        public function editarAction(){
-                $this->view->breadcrumb = $this->view->translate("Porteiro >> Cadastrar nova senha");
-
-                // Parse do arquivo de formulário
-                $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
-                                              "/modules/porteiro/forms/editar.xml" );
-
-                // Cria objeto Snep_Form
-                $form = new Snep_Form($xml);
-                $form->setMethod('post');
-                $form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
-
-                // Verifica se existe dados sendo enviados via $_POST
-                // Se for verdadeiro, é porqyue o formulário foi submetido.
-                if ($this->_request->getPost()) {
-
-                        // Chama método isValid() é confronta os dados submetidos pelo formulário.
-                        $isValid = $form->isValid($_POST);
-
-                        // Caso tudo seja válido chama a classe (Model) para inserir o dado.
-                        if( $isValid ) {
-
-                                $senha1 =(int) $_POST['senha'];
-                                $senha2 = (int) $_POST['senhac'];
-                                $usuario = $_POST['nome'];
-                                $grupo = $_POST['group'];
-
-                                // Após remover ou nao dados redireciona para método index
-                                // Se as senhas forem iguais, ele vai redirecionar ao index
-                                if ($senha1 === $senha2)  {
-
-                                        Senha_Manager::adit($_POST);
-                                        $this->_redirect($this->getRequest()->getModuleName() . "/senha/index");
-                                }
-
-
-                                else {
-                                        $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
-                                }
-
-                        }
-                        else {
-                                $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
-                        }
-                }
-
-
-
-        public function indexAction(){
-                // Define a baseUrl para a rotina, utilizado nos links para demais rotinas na view
-                $this->view->url = $this->getRequest()->getModuleName() .'/'. $this->getRequest()->getControllerName();
-                $this->view->breadcrumb = $this->view->translate("Porteiro >> Inicio");
-        }
-
-        public function removerAction(){
-                $this->view->breadcrumb = $this->view->translate("Porteiro >> Cadastrar nova senha");
-
-                // Parse do arquivo de formulário
-                $xml = new Zend_Config_Xml( Zend_Registry::get("config")->system->path->base .
-                                              "/modules/porteiro/forms/remover.xml" );
-
-                // Cria objeto Snep_Form
-                $form = new Snep_Form($xml);
-                $form->setMethod('post');
-                $form->setEnctype(Zend_Form::ENCTYPE_URLENCODED);
-
-                // Verifica se existe dados sendo enviados via $_POST
-                // Se for verdadeiro, é porqyue o formulário foi submetido.
-                if ($this->_request->getPost()) {
-
-                        // Chama método isValid() é confronta os dados submetidos pelo formulário.
-                        $isValid = $form->isValid($_POST);
-
-                        // Caso tudo seja válido chama a classe (Model) para inserir o dado.
-                        if( $isValid ) {
-
-                                Senha_Manager::remove($_POST);
-                                $this->_redirect($this->getRequest()->getModuleName() . "/senha/index");
-
-
-
-                                else {
-                                        $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
-                                }
-
-                        }
-                        else {
-                                $this->_redirect($this->getRequest()->getModuleName(). "/senha/errorcadastrar");
-                        }
-                }
-
 }
-
 ?>
-
--- lib/Senha/Manager.php
+++ lib/Senha/Manager.php
@@ -3,59 +3,47 @@
 class Senha_Manager {
  
 	public static function add($data){
-
 	        $db = Zend_Registry::get('db');
 		$calendario =  date("Y-m-d H:i");
 	        $insert_data = array("senha" => $data['senha'], "usuario" => $data['nome'], "grupo" => $data['group'], "cadastro" => $calendario, "atualizado" => $calendario);
-
         	$db->beginTransaction();
-
         	try{
             		$db->insert('senha', $insert_data);
             		$db->commit();
-
         	}catch(Exception $e){
-
             	$db->rollback();
-
         	}
 	}
 
-
         public static function edit($data){
-
                 $db = Zend_Registry::get('db');
                 $calendario =  date("Y-m-d H:i");
-		$sql = 'SELECT cadastro FROM senha WHERE usuario = ' . $data['nome'];
-                $insert_data = array("senha" => $data['senha'], "usuario" => $data['nome'], "grupo" => $data['group'], 'cadastro' => $sql, "atualizado" => $calendario);
-		$usuario => $data['nome'];
-                $db->beginTransaction();
-	
-                try{
-                        $db->update('senha' ,$insert_data, "usuario = {$usuario}");
-                        $db->commit();
-
-                }catch(Exception $e){
-
-                $db->rollback();
-
-                }
-        }
-        public static function remove($data){
-                $db = Zend_Registry::get('db');
-		$usuario = $data['nome'];
+                $insert_data = array("senha" => $data['senha'], "usuario" => $data['nome'], "grupo" => $data['group'], "atualizado" => $calendario);
                 $db->beginTransaction();
                 try{
-			$db->delete('senha', array(
-					'usuario = ?' => $usuario));
+                        $db->update('senha' , $insert_data, "usuario = " . $data['nome']);
                         $db->commit();
                 }catch(Exception $e){
                 $db->rollback();
                 }
         }
 
+        public static function remove($nome) {
 
+	        $db = Zend_Registry::get('db');
+		$nome = (string) $nome;
+        	$db->beginTransaction();
 
+        	try {
+            		$db->delete('senha', "id = $nome");
+            		$db->commit();
+
+        	} catch (Exception $e) {
+            		//$db->rollBack();
+
+        	}	
+
+    	}
 
 
 
