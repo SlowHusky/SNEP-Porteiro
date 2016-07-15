e0b8cd91329dcbd3d4d68624526267d5d683786c
SlowHusky <slowhusky@gmail.com>
2016-07-15 12:00:44 -0300

    Melhora da interface

--- lib/Gerenciador/Manager.php
+++ lib/Gerenciador/Manager.php
@@ -15,33 +15,20 @@ class Gerenciador_Manager {
 
         public static function porteiroGetAllAtivo()
         {   
-                // Recupera instancia do Banco de dados no Zend_Registry
                 $db = Zend_Registry::get('db');
-
-                // Cria um objeto select
                 $select = $db->select()
-                     // Tabela do select e indices que deverão ser retornados.
                      ->from('tb_porteiro', array('id','ip', 'transporte', 'mac','nome', 'rele1', 'rele2', 'cadastro', 'atualizado'));
-
                 $stmt = $db->query($select);
                 $result = $stmt->fetchAll();
-
                 return $result;
         }   
         public static function porteiroGetByID($id)
         {
-                // Recupera instancia do Banco de dados no Zend_Registry
                 $db = Zend_Registry::get('db');
-
-                // Cria um objeto select
                 $select = $db->select()
-                     // Tabela do select e indices que deverão ser retornados.
                      ->from('tb_porteiro', array('id','ip', 'transporte', 'mac','nome', 'rele1', 'rele2', 'cadastro', 'atualizado'))
-                     // Método Where, estabelece critérios pra consulta.
                      ->where("id = $id")
-                     // Método Limit, limita o número de registros.
                      ->limit('1');
-
                 $stmt = $db->query($select);
                 $result = $stmt->fetchAll();
                 return $result;
@@ -49,38 +36,22 @@ class Gerenciador_Manager {
 
         public static function porteiroGetByMAC($mac)
         {
-                // Recupera instancia do Banco de dados no Zend_Registry
                 $db = Zend_Registry::get('db');
-
-                // Cria um objeto select
                 $select = $db->select()
-                     // Tabela do select e indices que deverão ser retornados.
                      ->from('tb_porteiro', array('id','ip', 'transporte', 'mac','nome', 'rele1', 'rele2', 'cadastro', 'atualizado'))
-                     // Método Where, estabelece critérios pra consulta.
                      ->where("mac = '$mac'");
-
                 $stmt = $db->query($select);
                 $result = $stmt->fetchAll();
                 return $result;
         }
 
-
-
-
         public static function grupoGetByID($id)
         {
-                // Recupera instancia do Banco de dados no Zend_Registry
                 $db = Zend_Registry::get('db');
-
-                // Cria um objeto select
                 $select = $db->select()
-                     // Tabela do select e indices que deverão ser retornados.
                      ->from('tb_grupos', array('id', 'grupo', 'cadastro', 'atualizado'))
-                     // Método Where, estabelece critérios pra consulta.
                      ->where("id = $id")
-                     // Método Limit, limita o número de registros.
                      ->limit('1');
-
                 $stmt = $db->query($select);
                 $result = $stmt->fetchAll();
                 return $result;
@@ -88,23 +59,14 @@ class Gerenciador_Manager {
 
         public static function grupoGetByNome($grupo)
         {
-                // Recupera instancia do Banco de dados no Zend_Registry
                 $db = Zend_Registry::get('db');
-
-                // Cria um objeto select
                 $select = $db->select()
-                     // Tabela do select e indices que deverão ser retornados.
                      ->from('tb_grupos', array('id', 'grupo', 'cadastro', 'atualizado'))
-                     // Método Where, estabelece critérios pra consulta.
                      ->where("grupo = '$grupo'");
-
                 $stmt = $db->query($select);
                 $result = $stmt->fetchAll();
                 return $result;
         }
-
-
-
         
 	public static function getDatabase($tabela)
 	{
@@ -271,8 +233,8 @@ class Gerenciador_Manager {
 		{
 			$porteiro = self::porteiroGetByMAC($valor);
 			
-			echo "ID Grupo: " . $grupo[0]['id'];
-			echo "ID Porteiro: " . $porteiro[0]['id'];
+			echo "ID Grupo: " . $grupo[0]['id'] . "          " ;
+			echo "ID Porteiro: " . $porteiro[0]['id'] . "            ";
 
 	                $status = self::verificaSeExiste($grupo[0]['id'], $porteiro[0]['id'], 'tb_porteirogrupos');
 			if ($status == false){ 
